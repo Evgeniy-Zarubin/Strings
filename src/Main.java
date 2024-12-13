621320d09cc32479837513e1185b2d3001130d17
Evgeniy-Zarubin <Uncontrollable69@gmail.com>
2024-12-13 11:52:03 +1000

    on Check String_1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-      //Задача 1
+        //Задача 1
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
@@ -8,13 +8,12 @@ public class Main {
 
         System.out.println("Ф.И.О. сотрудника - " + fullName);
 
-      //Задача 2
+        //Задача 2
         System.out.println(fullName.toUpperCase());
 
-     //Задача 3
+        //Задача 3
         fullName = "Иванов Семён Семёнович";
         fullName = fullName.replace("ё", "е");
         System.out.println("Ф.И.О. сотрудника - " + fullName);
-
     }
 }
