bd73534de2a32839afe25aeb1c2c265107a6ddd0
Evgeniy-Zarubin <Uncontrollable69@gmail.com>
2024-12-13 11:51:41 +1000

    on Check String_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-   s
+      //Задача 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "Ivanov Ivan Ivanovich";
+
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+
+      //Задача 2
+        System.out.println(fullName.toUpperCase());
+
+     //Задача 3
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+
     }
 }
